123df7b4cc926f1ca4ca938530a2098e2f35c160
silascorreia8 <silas8_silva@hotmail.com>
2025-11-18 14:10:08 -0300

    build: Class=FuncionarioPJ

--- src/FuncionarioPJ.java
+++ src/FuncionarioPJ.java
@@ -1,4 +1,41 @@
 
 public class FuncionarioPJ extends Funcionario{
     
+    private String cnpj;
+    private double valorContrato;
+    
+    public FuncionarioPJ(String nome, String matricula, String cnpj, double valorContrato) {
+        
+        super(nome, matricula, valorContrato);
+        this.cnpj = cnpj;
+        this.valorContrato = valorContrato;
+        
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public double getValorContrato() {
+        return valorContrato;
+    }
+
+    public void setValorContrato(double valorContrato) {
+        this.valorContrato = valorContrato;
+    }
+    
+    @Override
+    public double calculaSalario() {
+        return valorContrato;
+    }
+    
+    @Override
+    public String getBeneficios() {
+        return "Não possui beneficios";
+    }
+    
 }
